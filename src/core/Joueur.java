1de08d6620cb5bce9645e45156b2f98671a7ed27
Apache <tj.thomas.jeantet@gmail.com>
2015-12-06 20:04:46 +0100

    renvoi test

--- src/core/Joueur.java
+++ src/core/Joueur.java
@@ -19,7 +19,7 @@ public abstract class Joueur{
     protected Console console;
     
     public Joueur(boolean humain) {
-    	nbrPoints=1;
+    	nbrPoints=0;
     	nbrGraines=0;
     	nbrMenhirs=0;
         protecChien = 0;
