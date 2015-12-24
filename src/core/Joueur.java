099415913bde89af837763807edffaec55cc80ed
Buzzrage <marius95@live.fr>
2015-12-24 16:48:51 +0100

    Revert "correction javadoc joueur"
    This reverts commit 4016052ba5a0e1db810ff03eecc2b96a8a8f7436.

--- src/core/Joueur.java
+++ src/core/Joueur.java
@@ -23,6 +23,10 @@ import java.util.*;
  *@see core.JoueurIA
  *@see core.ChoixJoueur
  */
+/**
+ * @author Marius
+ *
+ */
 public abstract class Joueur implements Observer{
 
     private final int nbr;
